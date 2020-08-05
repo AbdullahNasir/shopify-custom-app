ee3bdf70696a9deabd49b58b1c746a41b30d9647
Haseeb Anwar <haseebanwar99@gmail.com>
2020-08-05 11:38:39 +0500

    Added liquid templating engine

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { Liquid } = require('liquidjs');
 const connectDB = require('./utils/db');
+
+const fs = require('fs');
+const path = require('path');
 
 // load env variables
 dotenv.config();
@@ -12,6 +16,13 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const engine = new Liquid();
+
+// register liquid engine
+app.engine('liquid', engine.express());
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'liquid'); // set liquid to default
+
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +30,14 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   console.log('GET Home page');
   res.send('GET Home page');
+});
+
+app.get('/test', (req, res) => {
+  res.set('Content-Type', 'application/liquid');
+  // const file = fs.readFileSync(path.join(__dirname, 'test.liquid'), 'utf8');
+  // console.log('file', file);
+  // res.send(file);
+  res.render('test');
 });
 
 app.use('/shopify', require('./routes/shopify'));
