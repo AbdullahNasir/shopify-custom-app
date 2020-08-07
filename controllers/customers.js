ac547ddeea2c3b0789f8d3c1c6bdc510fe58fc26
Haseeb Anwar <haseebanwar99@gmail.com>
2020-08-07 12:39:38 +0500

    Added redirect_uri for otp verification

--- controllers/customers.js
+++ controllers/customers.js
@@ -5,10 +5,26 @@ const Customer = require('../models/Customer');
 exports.sendOTP = async (req, res) => {
   try {
     // grab required data
-    const { first_name, last_name, email, phone, password, shop } = req.body;
+    const {
+      first_name,
+      last_name,
+      email,
+      phone,
+      password,
+      shop,
+      redirect_uri,
+    } = req.body;
 
     // validation
-    if (!first_name || !last_name || !email || !phone || !password || !shop) {
+    if (
+      !first_name ||
+      !last_name ||
+      !email ||
+      !phone ||
+      !password ||
+      !shop ||
+      !redirect_uri
+    ) {
       return res.status(400).send('Missing Required Parameters');
     }
 
@@ -25,7 +41,9 @@ exports.sendOTP = async (req, res) => {
     // TODO send otp to given phone number
 
     // redirect user to otp verification page
-    res.redirect(`https://${shop}/apps/verify-otp?id=${newCustomer._id}`);
+    res.redirect(
+      `https://${shop}/apps/verify-otp?id=${newCustomer._id}&redirect_uri=${redirect_uri}`
+    );
   } catch (error) {
     console.log('error', error);
     return res.status(500).send('Internal Server Error');
@@ -71,13 +89,13 @@ exports.verifyOTP = async (req, res) => {
 };
 
 exports.showOTPVerificationPage = (req, res) => {
-  const { id, shop } = req.query;
+  const { id, shop, redirect_uri } = req.query;
 
   // validation
-  if (!id || !shop) {
+  if (!id || !shop || !redirect_uri) {
     return res.status(400).send('Missing Required Parameters');
   }
 
   res.set('Content-Type', 'application/liquid');
-  return res.status(200).render('otp-verification', { id, shop });
+  return res.status(200).render('otp-verification', { id, shop, redirect_uri });
 };
